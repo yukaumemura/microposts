c4e76859f515c53835d43329753eef13be14dcce
YUKA Umemura <jackbaisonrunashaf@yahoo.co.jp>
2017-01-18 06:24:35 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,24 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
-
+  
+  def edit
+  end
+  
+  def update
+    if  @user.update(user_params)
+      # 保存に成功した場合はトップページへリダイレクト
+      redirect_to root_path , notice: 'メッセージを編集しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      flash[:success] = "ログインされました"
       redirect_to @user
     else
       render 'new'
@@ -24,7 +37,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+    params.require(:user).permit(:name, :email, :password,:password_confirmation,:area,:profile)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :area, presence: true, length: { maximum: 50 } 
+  validates :profile, presence: true, length: { maximum: 200 } 
   has_secure_password
 end
