37918efdaf2c1c6f7fd1cfd720d24997e20ab7b3
YUKA Umemura <jackbaisonrunashaf@yahoo.co.jp>
2017-01-18 09:44:27 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,15 +14,18 @@ class UsersController < ApplicationController
   def edit
   end
   
-  def update
-    if  @user.update(user_params)
+ def update
+  @user = User.find(params[:id])
+   if  @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      redirect_to root_path 
+      flash[:success] = "プロフィールを更新しました"
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
   end
+
   
   def create
     @user = User.new(user_params)
